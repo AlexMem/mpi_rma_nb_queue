fe7ba94ed9a17d305a376dd3baacbbbcac646fc0
Aleksandr Burachenko <ss47305@gmail.com>
2020-05-26 18:13:05 +0300

    improved scripts for compilation and job execution

--- mpi_rma_nonblocking_queue/job.sh
+++ mpi_rma_nonblocking_queue/job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #PBS -l walltime=00:01:00
-#PBS -l select=2:ncpus=4:mpiprocs=4:mem=2000m,place=free
+#PBS -l select=1:ncpus=4:mpiprocs=4:mem=2000m,place=free
 #PBS -m n
 
 cd $PBS_O_WORKDIR
@@ -13,4 +13,5 @@ echo 'File $PBS_NODEFILE:'
 cat  $PBS_NODEFILE
 echo 
 
+mpicxx -O3 -o rma_nb_queue_exec.out include/utils.c include/mpigclock.c include/hpctimer.c rma_nb_queue.cpp
 mpirun -hostfile $PBS_NODEFILE -np $MPI_NP ./rma_nb_queue_exec.out
